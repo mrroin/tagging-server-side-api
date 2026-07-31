efa450c9924fd05b2a51bc9b40adb9c5ea760711
Rogelio Méndez <rogeliomendez@MacBook-Air-de-Rogelio.local>
2026-07-31 13:48:11 -0500

    version logs

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
   await app.listen(`${process.env.PORT}`, "0.0.0.0");
   console.log(
     "🚀 Server listen in: ",
-    `${process.env.PORT}, version 0.0.1 300726 15:18`
+    `${process.env.PORT}, version 0.0.1 310726 13:47`
   );
 }
 bootstrap();
